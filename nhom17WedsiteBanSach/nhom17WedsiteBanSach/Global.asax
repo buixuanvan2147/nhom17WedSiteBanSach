f5dd767ddd09c26c8c73bc24a133d81980750611
buixuanvan2147 <22115053122147@sv.ute.udn.vn>
2024-11-20 17:11:14 +0700

    1st push

---
+++ nhom17WedsiteBanSach/nhom17WedsiteBanSach/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="nhom17WedsiteBanSach.MvcApplication" Language="C#" %>
---
+++ nhom17WedsiteBanSach/nhom17WedsiteBanSach/Global.asax.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using DotNetEnv;
+
+namespace nhom17WedsiteBanSach
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            // Tải file .env
+            Env.Load();
+
+            // Lấy chuỗi kết nối cơ sở dữ liệu từ file .env
+            string connectionString = Env.GetString("DB_CONNECTION");
+
+            // Cập nhật chuỗi kết nối trong Web.config
+            System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString = connectionString;
+
+
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
